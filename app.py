6377630dbb5b582f76dfe8f2a82736028090d2df
luisoria <luisoria@users.noreply.gitea.local>
2026-06-12 21:31:28 -0400

    Fix: listen on 0.0.0.0 for Docker accessibility

--- app.py
+++ app.py
@@ -414,4 +414,4 @@ def health():
 
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=8088, threaded=True, debug=True)
+    app.run(host="0.0.0.0", port=8088, threaded=True, debug=False)
